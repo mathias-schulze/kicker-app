89972e20e910304c09d4242950c4e1c1404ce9d5
Mathias Schulze <mathias-schulze@web.de>
2019-12-23 18:30:38 +0100

    adding Google Firebase Firestore as database

---
+++ src/main/java/de/msz/kickerapp/service/FirestoreService.java
@@ -0,0 +1,82 @@
+package de.msz.kickerapp.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.EventListener;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreException;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+import de.msz.kickerapp.domain.Player;
+
+@Service
+public class FirestoreService {
+
+    private final Logger log = LoggerFactory.getLogger(FirestoreService.class);
+
+private Firestore db;
+    
+    private Map<String, Player> players = new HashMap<>();
+    
+	public Map<String, Player> getPlayers() {
+		return players;
+	}
+	
+	@PostConstruct
+	private void init() throws IOException {
+		db = getFirestore();
+		initPlayers();
+	}
+	
+	private Firestore getFirestore() throws IOException {
+		
+		File firestoreConfFile = new File("src\\\\main\\\\resources\\\\mszkicker-firestore-conf.json");
+		// local configuration file
+		if (firestoreConfFile.exists()) {
+			InputStream serviceAccount = new FileInputStream(firestoreConfFile);
+			GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+			FirebaseOptions options = new FirebaseOptions.Builder()
+			    .setCredentials(credentials)
+			    .build();
+			FirebaseApp.initializeApp(options);
+		// environment variable FIREBASE_CONFIG for cloud deployment
+		} else {
+			FirebaseApp.initializeApp();
+		}
+		
+		return FirestoreClient.getFirestore();
+	}
+	
+	private void initPlayers() {
+		
+		db.collection("spieler").addSnapshotListener(new EventListener<QuerySnapshot>() {
+			
+			@Override
+			public void onEvent(QuerySnapshot querySnapshot, FirestoreException error) {
+				
+				querySnapshot.getDocumentChanges().forEach(change -> {
+					QueryDocumentSnapshot document = change.getDocument();
+					Player player = new Player();
+					player.setId(document.getId());
+					player.setName(document.getString("name"));
+					players.put(player.getId(), player);
+				});
+			}
+		});
+	}}
--- src/main/java/de/msz/kickerapp/web/rest/DashboardResource.java
+++ src/main/java/de/msz/kickerapp/web/rest/DashboardResource.java
@@ -1,11 +1,16 @@
 package de.msz.kickerapp.web.rest;
 
+import java.util.Collection;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.msz.kickerapp.domain.Player;
+import de.msz.kickerapp.service.FirestoreService;
 import de.msz.kickerapp.web.rest.dashboard.Game;
 import de.msz.kickerapp.web.rest.dashboard.GameList;
 
@@ -15,12 +20,12 @@ import de.msz.kickerapp.web.rest.dashboard.GameList;
 @RestController
 @RequestMapping("/api/dashboard")
 public class DashboardResource {
-
+	
     private final Logger log = LoggerFactory.getLogger(DashboardResource.class);
-
-    /**
-    * GET lastGames
-    */
+    
+    @Autowired
+    private FirestoreService db;
+    
     @GetMapping("/last-games")
     public GameList lastGames() {
     	
@@ -38,5 +43,9 @@ public class DashboardResource {
     	
         return games;
     }
-
+    
+    @GetMapping("/ranking")
+    public Collection<Player> getRanking() {
+    	return db.getPlayers().values();
+    }
 }
